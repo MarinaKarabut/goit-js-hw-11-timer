6e586141388bd51afe41f2e45b39a5ac3fbc17ab
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-24 23:23:52 +0200

    таймер обратного отсчета

---
+++ index.js
@@ -0,0 +1,61 @@
+const countdownTimer =document.getElementById('timer-1')
+const timerDate = countdownTimer.querySelector('[data-value="days"]')
+const timerHour = countdownTimer.querySelector('[data-value="hours"]')
+const timerMinutes = countdownTimer.querySelector('[data-value="mins"]')
+const timerSeconds = countdownTimer.querySelector('[data-value="secs"]')
+
+class CountdownTimer {
+  constructor({selector, targetDate}){
+    this.selector = selector;
+    this.targetDate = targetDate;
+  
+}
+
+intervalId = setInterval(()=> {
+  const currentTime = Date.now()
+  const deltaTime = this.targetDate - currentTime;
+  this.getTimeComponents(deltaTime)
+  this.finishCountdownTimer(deltaTime)
+ 
+}, 1000)
+
+getTimeComponents (deltaTime){
+  const days = this.pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
+  const hours = this.pad(Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = this.pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = this.pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
+  timerDate.textContent = `${days}`
+  timerHour.textContent = `${hours}`
+  timerMinutes.textContent = `${mins}`
+  timerSeconds.textContent = `${secs}`
+
+}
+pad(value){
+  return String(value).padStart(2, '0')
+  }
+
+
+finishCountdownTimer(deltaTime){
+  if (deltaTime < 0){
+    clearInterval(this.intervalId)
+    countdownTimer.textContent = 'The End'
+  }
+}
+
+}
+
+
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2021'),
+});
+
+
+
+
+
+
+
+
+
